ca91c2e6de0e7a6c66f9929581f4a4e126eb1e1d
sophos <sophos@192.168.0.32>
2017-02-13 17:23:25 -0700

    Updated fieldcorr error checking

--- Field Correlation/fieldcorr.m
+++ Field Correlation/fieldcorr.m
@@ -1,10 +1,19 @@
-function[corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, MC, noiseType, varargin)
+function[corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, varargin)
 %% Determines the correlation coefficients of a time series with a field, 
-% and performs a Monte Carlo significance test on the correlation.
+% and tests for significance of the correlations.
 % 
-% [corrmap, pmap , isSig] = fieldcorr(ts, field, p, MC, noiseType)
+% [corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, MC, noiseType)
+% computes field-timeseries correlation coefficients and tests whether 
+% correlation coefficients remain significant at a given level given a
+% finite number of tests AND spatial reduction of degrees of freedom in a
+% highly correlated field.
 %
-% [corrmap, pmap , isSig] = fieldcorr(ts, field, p, MC, noiseType, fieldDim)
+% [...] = fieldcorr(ts, field, pvals, 'noSpatial')
+% suppresses significance testing with respect to spatial reduction of
+% degrees of freedom
+%
+% [...] = fieldcorr(..., fieldDim)
+% performs calculations along a specified dimension of the field.  
 %
 % ----- Inputs -----
 % 
@@ -12,20 +21,21 @@ function[corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, MC, noiseType, vara
 %
 % field: A field. The time series at individual points in the field should
 %   have the same length as ts. Field may be n-dimensional. By default,
-%   fieldcorr assumes that the time series is stored along the first
+%   fieldcorr assumes that the time series is along the first
 %   dimension. (See the fieldDim argument if this is not the case)
 %
 % pvals: A vector containing the p values of interest. (e.g. p = 0.05 
 %   corresponds to the 95% confidence level. Correlation coefficients are
 %   tested for significance at pval levels using Monte Carlo sampling.
 %
+% 'noSpatial': A flag to suppress significance testing of  
+%
 % noisetype: The type of noise in the Monte Carlo time series.
 %    'white': White Gaussian noise
 %    'red': Red lag-1 autocorrelated noise with added Gaussian white noise
 %
-% MC: The Monte Carlo number
-%
-% *** Optional Inputs ***
+% MC: The number of Monte Carlo simulations to use in spatial significance
+%   testing.
 %
 % fieldDim: (Default = 1) A scalar, specifies the dimension of the field on
 %   which to perform the field correlation.
@@ -49,74 +59,87 @@ function[corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, MC, noiseType, vara
 % Jonathan King, 2017
 
 
-%% Perform some initial error checking, get the number of dimensions.
-[sField] = setup(ts, field, pvals, varargin);
+%% Perform some initial error checking, get the number of dimensions. Make
+% the time series a column vector.
+
+[testSpatial, noiseType, MC, fieldDim] = parseInputs( varargin);
+
+[fieldDim, ts, testSpatial] = setup(ts, field, pvals, varargin);
 
 
 %% Perform the actual correlation
 
-% Reshape the field to a 2D matrix and the time series to a column vector
-field = reshape(field, sField(1), prod(sField(2:end)) );
-if isrow(ts)
-    ts = ts';
-end
+% Reshape the field into a 2D matrix along the dimension of interest
+[field, dSize, dOrder] = dimNTodim2(field, fieldDim);
 
 % Perform the correlation
 [corrmap, pmap] = corr(ts, field);
 
 % Reshape the correlation map to the shape of the original field
-corrmap = reshape( corrmap, [1 sField(2:end)] );
-
-
-%% Perform significance tests (before reshaping the pmap)
+corrmap = dim2TodimN( corrmap, [1 dSize(2:end)], dOrder);
 
 % p values of NaN are not actual tests, just missing data. Thus, remove
 % them from statistical considerations.
 ptests = pmap(~isnan(pmap));
 
 % Now reshape the pmap
-pmap = reshape( pmap, [1 sField(2:end)]);
+pmap = dim2TodimN( pmap, [1 dSize(2:end)], dOrder);
 
-% Check that the finite number of tests do not cause the true significance
-% of the tests to fall below the desired significance level.
+
+%% Perform statistical tests
+
+
+%% Test 1 - Do the finite number of tests cause true significance to fall 
+% below the desired significance level.
+
 nTests = size(ptests, 2);
-nPass = sum( ptests<=pvals );
+npvals = length(pvals);
+nPass = NaN(npvals, 1);
 
+% Get the number of correlations that initially pass each desired
+% significance level.
+for k = 1:npvals
+    nPass(k) = sum( ptests <= pvals(k) );
+end
+
+% Check if finite tests are still significant at each significance level
 isSig = finiteTestsAreSig( nTests, nPass, pvals);
 
-if ~isSig
-    % If the tests are not significant, stop
+% If none of the p values are significant, stop. There is no need to
+% account for spatial reduction of degrees of freedom because none of them
+% are significant
+if ~any( isSig )
     return;
-      
-else
-    % The number of passed tests is greater than the number expected to
-    % occur randomly.
-    
-    % However, reduced degrees of freedom (from spatial correlation) could
-    % raise the number expected to occur randomly. Test this possibility
-    % with a Monte Carlo simulation.
+end
+
+
+%% Test 2 - Check if spatial correlation reduces significance below 
+% desired levels
+
+% Only run if desired...
+if testSpatial
     
     % Begin by creating a set of random time series.
-    randTS = buildRandomTS(ts, MC, noiseType);
-        
+    randTS = randNoiseSeries(ts, MC, noiseType);
+
     % Scale to the standard deviation of the time series
     randTS = randTS * std(ts);
 
-    % Get the correlation of each time series with the data from each point
-    % in the field
+    % Get the correlation significace of each Monte Carlo time series with
+    % the data from each point in the field
     [~, mcPvals] = corr(randTS, field);
-    
-    % Get the number of points that pass the significance test for each
-    % time series across the field.
+
+    % Get the number of points for each random time series that pass the
+    % significance test with the field.
     mcPass = nansum( mcPvals < pvals, 2);
 
     % Sort the number of passed tests
     mcPass = sort( mcPass);
-    
-    % Get the index of minimum of the p largest number of passed tests
+
+    % Get the index of the p largest number of passed tests
     sigDex = ceil(pvals*MC);
     mcNPass = mcPass(sigDex);
-    
+
     % Compare this number of passed tests to the number passed by the time
     % series.
     if nPass < mcNPass
@@ -126,13 +149,48 @@ else
     else
         isSig = true;
     end
-    
 end
 
 end
 
 
 % ----- Helper Functions -----
+
+% Reads the input arguments
+function[testSpatial, noiseType, MC, fieldDim] = parseInputs( inArgs )
+
+% Set default fieldDim
+fieldDim = 1;
+
+% See if the spatial testing is suppressed
+if isempty(inArgs)
+    error('Insufficient input arguments');
+end
+if strcmpi(inArgs{1}, 'noSpatial')
+    testSpatial = false;
+    noiseType = NaN;
+    MC = NaN;
+    
+    % Check for a fieldDim argument
+    if ~isempty(inArgs{2})
+        fieldDim = inArgs{2};
+    end
+    
+elseif length(inArgs) < 4
+    testSpatial = true;
+    MC = inArgs{1};
+    noiseType = inArgs{2};
+    
+    % Check for a fieldDim argument
+    if ~isempty(inArgs{3})
+        fieldDim = inArgs{3};
+    end
+    
+else
+    error('Too many input arguments');
+end  
+end
+
 
 % Error checking and sizes
 function[sField] = setup(ts, field, pvals, inArgs)
@@ -178,41 +236,3 @@ elseif (pvals <= 0 || pvals >= 1)
 end
 
 end
-    
-function[randTS] = buildRandomTS(ts, MC, noise)
-% Build a set of random time series with the appropriate noise type.
-
-% Get the length of the time series
-lts = length(ts);
-    
-switch noise
-    
-    case 'white'
-        randTS = randn( lts, MC );
-        
-    case 'red'
-        % Preallocate
-        randTS = NaN( lts, MC);
-        
-        % Get the lag-1 autocorrelation
-        ar1 = corr( ts(1:end-1), ts(2:end));
-        
-        % Initialize the first row
-        randTS(1,:) = randn(1,MC);
-        
-        % Calculate autocorrelation through matrix. Add random noise
-        for k = 1:lts-1
-            randTS(k+1,:) = (ar1 * randTS(k,:)) + randn(1,MC);
-        end
-        
-        % Standardize so later scaling is correct
-        randTS = zscore(randTS);
-        
-    otherwise
-        error('Unrecognized noise type');
-end
-
-end
-
-
-
