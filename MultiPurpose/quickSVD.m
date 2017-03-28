ddf50d366ee25763df2d6a87b0c04c3ddc3935f8
JonKing93 <jonking93@gmail.com>
2017-03-28 10:33:13 -0700

    Finished quickSVD

--- MultiPurpose/quickSVD.m
+++ MultiPurpose/quickSVD.m
@@ -28,42 +28,24 @@ function[eigvals, eigvecs] = quickSVD(C, varargin)
 % Parse Inputs
 [svdFunc, svdsArg] = parseInputs( varargin(:));
 
-
-% Error check, parse inputs, get number of matrices
-[numMat, decompType, decompArg] = setup(C, varargin{:});
+% Error check
+errCheck(C);
 
 % Run the SVD on each matrix
-for k = 1:numMat
-    
-    switch decompType
-        case 'svd'
-            if strcmp(decompArg,'all')
-                [~,S,V] = svd(C(:,:,k));           
-            else
-                [~,S,V] = svd(C(:,:,k), 0);
-            end
-            
-        case 'svds'
-            if isscalar(decompArg)
-                [~,S,V] = svds(C(:,:,k), decompArg);
-            else
-                [~,S,V] = svds(C(:,:,k));
-            end 
-    end
-   
-    % Preallocate if this is the first run
-    if k==1
-        eigvals = NaN( length(diag(S)), numMat);
-        eigvecs = NaN( [size(V), numMat]);
-    end
-                  
-    % Pull the eigenvalues off of the diagonal
-    eigvals(:,k) = diag(S);
-    
-    % Set the eigenvectors so that the majority of loadings are positive
-    eigvecs(:,:,k) = posColSign(V);
-        
+switch svdFunc
+    case 'svd'
+        [~,S,V] = svd(C);
+
+    case 'svds'
+        [~,S,V] = svds(C, svdsArg); 
 end
+
+% Pull the eigenvalues off of the diagonal
+eigvals = diag(S);
+
+% Set the eigenvectors so that the majority of elements are positive
+eigvecs = posColSign(V);
+        
 end
     
     
@@ -95,59 +77,13 @@ if ~isempty(inArgs)
 end
 end
           
-
-
-function[nMat, svdType, nEigs] = setup(C,varargin)
-%% Error checking and determination of input specifications
-
-% Ensure C is 3D 
-if ndims(C) > 3
-    error('quickSVD is for 3D matrices only');
+function[] = errCheck(C)
+% C is a matrix
+if ~ismatrix(C)
+    error('C must be a matrix');
+% No NaN
+elseif any(isnan(C))
+    error('C may not contain NaN');
+end
 end
 
-% Ensure no NaNs are in C
-if NaNcheck(C)
-    error('C may not contain NaN entries');
-end
-
-% Get the number of matrix stacks
-nMat = size(C,3);
-
-% Set defaults
-svdType = 'svd';
-nEigs = 'all';
-
-% Check the additional specifications
-if ~isempty(varargin)
-    if length(varargin) > 2
-        error('Too many inputs');
-    else
-        % SVD type
-        if strcmp( varargin{1}, 'svd')
-            % Do nothing, this is the default
-        elseif strcmp( varargin{1}, 'svds')
-            svdType = 'svds';
-            nEigs = 'econ';
-        else
-            error('Unrecognized svd type');
-        end
-        % nEigs
-        if length(varargin) == 1
-            % Do nothing
-        elseif strcmpi( varargin{2}, 'all') && strcmp(svdType, 'svd')
-            % Do nothing
-        elseif strcmpi( varargin{2}, 'econ')
-            nEigs = 'econ';
-        elseif isscalar(varargin{2}) && strcmp(svdType, 'svd')
-            error('Use the svds switch to find a specific number of singular values');
-        elseif strcmpi(varargin{2}, 'all')
-            error('Use the svd switch to perform a full decomposition');
-        elseif isscalar(varargin{2})
-            nEigs = varargin{2};
-        else
-            error('Unrecognized input');
-        end
-    end
-end
-
-end
