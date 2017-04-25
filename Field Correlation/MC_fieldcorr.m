502d84ff2ad388387ab8e1ca7c22276bd609c1b1
JonKing93 <jonking93@gmail.com>
2017-04-25 15:17:19 -0700

    Building fieldcorr_Analysis, split significance testing into MC_fieldcorr

---
+++ Field Correlation/MC_fieldcorr.m
@@ -0,0 +1,173 @@
+function[varargout] = MC_fieldcorr(ts, field2D, MC, noiseType, p, varargin)
+%% Performs a Monte Carlo test on a field correlation to determine if 
+% correlations remain significant above a given level after accounting for
+% spatial interdependence of field sites.
+%
+%
+% [nNeeded, iterNPassed, iterTrueConf] = MC_fieldcorr(ts, field2D, MC, noiseType, p)
+% Computes the number of passed significance tests required for the field
+% correlation to remain above the significance level p. Performs a test on
+% the convergence on the Monte Carlo method.
+%
+% [isSig, iterNPassed, iterTrueConf] = MC_fieldcorr(ts, field2D, MC, noiseType, p, nPassed)
+% Returns a boolean for whether the given number of passed tests remains
+% above the significance level p. Performs a convergence test.
+%
+% [...] = MC_fieldcorr(..., 'corrArgs', {corrArgs})
+% Performs the test using alternative parameters for the "corr" function. 
+% See the help section of "corr" for details.
+%
+% [nNeeded/isSig] = MC_fieldcorr(..., 'noConvergeTest')
+% Blocks testing of the Monte Carlo convergence test.
+%
+%
+% ----- Inputs -----
+%
+% ts: A time series vector. Cannot contain NaN.
+%
+% field2D: A field. The time series at individual points in the field should
+%       have the same length as ts. Field must be 2 dimensional, and ts
+%       must run parallel to the first dimension.
+%
+% MC: The number of Monte Carlo iterations to perform
+%
+% noiseType: The type of noise to use during the test
+%       'white': white Gaussian noise
+%       'red': lag-1 autocorrelated noise with added Gaussian white noise
+%
+% p: The significance level that the significance tests are performed against
+%
+% nPassed: The number of significance tests that passed the p significance level
+%
+% corrArgs: A cell containing alternative parameters for the "corr"
+%       function, see the "corr" help section for details.
+%
+%
+% ----- Outputs -----
+%
+% nNeeded: The number of passed tests required to maintain significance at
+%       the p significance level.
+%
+% isSig: A boolean for whether the given number of passed tests maintains
+%       significance at the p significance level.
+%
+% iterNPassed: The number of passed significance tests that must be
+%       exceeded to maintain significance for each Monte Carlo iteration.
+%
+% iterTrueConf: The true confidence level used for each iteration.
+
+% Parse inputs, do an error check
+[nPassed, corrArgs, convergeTest] = parseInputs(varargin{:});
+errCheck(ts, field2D, MC, p);
+
+% Create a set of random time series with the desired noise properties
+randTS = randNoiseSeries(noiseType, ts, MC);
+
+% Get the significance of the correlation of each random series with each
+% point on the field. The actual correlation coefficient is not needed here
+[~, mcPval] = corr(randTS, field2D, corrArgs{:});
+
+% Get the number of correlations that pass the significance level for each
+% series.
+nPass = nansum(mcPval < p, 2);
+
+% If testing Monte Carlo convergence...
+if convergeTest
+    
+    % Preallocate the convergence data
+    iterNPassed = NaN(MC,1);
+    iterTrueConf = NaN(MC,1);
+    
+    % For each iteration...
+    for k = 1:MC
+        
+        % ... sort the number of passed tests for the first k series
+        iterPass = sort( nPass(1:k) );
+        
+        % Get the threshold index of the first significant element for the
+        % current iteration.
+        iterThresh = ceil( (1-p)*k );
+        iterTrueConf(k) = iterThresh / k;
+        
+        % Get the number of required passed tests for the current iteration
+        iterNPassed(k) = iterPass(iterThresh);
+    end
+    
+    % Get the number of passed tests needed
+    nNeeded = iterNPassed(end);
+    
+else % ... not testing convergence
+    % Sort the number of passed tests and get the threshold index
+    nPass = sort(nPass);
+    thresh = ceil( (1-p)*MC );
+    
+    % Get the number of passed tests at the significance level
+    nNeeded = nPass(thresh);
+end
+
+% Return the desired output variable
+if isnan(nPassed)
+    varargout{1} = nNeeded;
+else
+    varargout{1} = (nPassed > nNeeded); % ... the isSig arg
+end
+
+if convergeTest
+    varargout(2:3) = {iterNPassed, iterTrueConf};
+end
+    
+end
+
+% ----- Helper Methods -----
+function[] = errCheck(ts, field2D, MC, p)
+
+if ~isvector(ts)
+    error('ts must be a vector');
+elseif hasNaN(ts)
+    error('ts cannot contain NaN');
+elseif ~ismatrix(field2D)
+    error('field2D must be a 2D matrix');
+elseif MC<1 || mod(MC,1)~=0
+    error('MC must be a positive integer');
+elseif p<=0 || p>=1
+    error('p must be on the interval (0,1)');
+elseif length(ts) ~= size(field2D,1)
+    error('The time series must run parallel to the first dimension of field2D');
+end
+
+end
+
+function[nPassed, corrArgs, convergeTest] = parseInputs(varargin)
+inArgs = varargin;
+
+% Set defaults
+nPassed = NaN;
+corrArgs = {};
+convergeTest = true;
+
+if ~isempty(inArgs)
+    
+    isCorrArg = false;
+    
+    for k = 1:length(inArgs)
+        arg = inArgs{k};
+        
+        % The nPassed arg
+        if k==1 && isscalar(k)
+            if mod(arg,1)==0 && arg>=0
+                nPassed = arg;
+            else
+                error('nPassed must be a non-negative integer');
+            end
+        elseif isCorrArg
+            corrArgs = arg;
+        elseif strcmpi(arg, 'corrArgs')
+            isCorrArg = true;
+        elseif strcmpi(arg, 'noConvergeTest')
+            convergeTest = false;
+        else
+            error('Unrecognized Input');
+        end
+    end
+end
+end
--- Field Correlation/fieldcorr.m
+++ Field Correlation/fieldcorr.m
@@ -1,210 +1,106 @@
-function[corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, varargin)
-%% Determines the correlation coefficients of a time series with a field, 
-% and tests for significance of the correlations.
+function[corrmap, pmap] = fieldcorr(ts, field, varargin)
+%% Determines the correlation coefficients of a time series with a field,
+% as well as the significance of the correlation.
 % 
-% [corrmap, pmap, isSig] = fieldcorr(ts, field, pvals, MC, noiseType)
-% computes field-timeseries correlation coefficients and tests whether 
-% correlation coefficients remain significant at a given level given a
-% finite number of tests AND spatial reduction of degrees of freedom in a
-% highly correlated field.
+% [corrmap, pmap] = fieldcorr(ts, field)
+% computes field correlations for a time series. Returns correlation
+% coefficients and significance p values.% highly correlated field.
 %
-% [...] = fieldcorr(ts, field, pvals, 'noSpatial')
-% suppresses significance testing with respect to spatial reduction of
-% degrees of freedom
+% [...] = fieldcorr(..., 'fieldDim', d)
+% performs calculations along a specified dimension of the field. By
+% default, correlations will be calculated along the first dimension.
 %
-% [...] = fieldcorr(..., fieldDim)
-% performs calculations along a specified dimension of the field.  
+% [...] = fieldcorr(..., 'corrArgs', {corrArgs})
+% Computes correlations using alternative parameters for the "corr"
+% function. See the help section of "corr" for details.
+%
 %
 % ----- Inputs -----
 % 
 % ts: a time series. This is a single vector.
 %
 % field: A field. The time series at individual points in the field should
-%   have the same length as ts. Field may be n-dimensional. By default,
-%   fieldcorr assumes that the time series is along the first
-%   dimension. (See the fieldDim argument if this is not the case)
+%       have the same length as ts. Field may be n-dimensional. By default,
+%       fieldcorr assumes that the time series runs parallel to the first dimension.
 %
-% pvals: A vector containing the p values of interest. (e.g. p = 0.05 
-%   corresponds to the 95% confidence level. Correlation coefficients are
-%   tested for significance at pval levels using Monte Carlo sampling.
+% d: A scalar specifying the dimension of the field on which to perform the
+%       correlation.
 %
-% 'noSpatial': A flag to suppress significance testing with respect to
-%   spatial loss of degrees of freedom.
+% corrArgs: A cell containing alternative parameters for the "corr"
+%       function, see the "corr" help section for details.
 %
-% noisetype: The type of noise in the Monte Carlo time series.
-%    'white': White Gaussian noise
-%    'red': Red lag-1 autocorrelated noise with added Gaussian white noise
-%
-% MC: The number of Monte Carlo simulations to use in spatial significance
-%   testing.
-%
-% fieldDim: (Default = 1) A scalar, specifies the dimension of the field on
-%   which to perform the field correlation.
 %
 % ----- Outputs -----
 %
 % corrmap: An array displaying the correlation coefficients at each point
-% on the field.
+%       on the field.
 %
 % pmap: Displays the p value of the correlation coefficient for each point
-% on the field.
-%
-% isSig: A boolean vector corresponding to each input p-value. A value of
-% true indicates a correlation is significant at the corresponding p-value.
-%
-% ----- Additional Reading -----
-%
-% Livezney and Chen (1983)
-%
-% -----
-% Jonathan King, 2017
+%       on the field.
 
 
-%% Perform some initial error checking, get the number of dimensions. Make
-% the time series a column vector.
-
-[testSpatial, noiseType, MC, fieldDim] = parseInputs( varargin);
-
-[ts] = setup(ts, field, pvals, fieldDim);
-
-
-%% Perform the actual correlation
+% Parse the inputs, do some error checking
+[fieldDim, corrArgs] = parseInputs( varargin{:});
+[ts] = errCheck(ts, field, fieldDim); % Also makes ts a column vector
 
 % Reshape the field into a 2D matrix along the dimension of interest
 [field, dSize, dOrder] = dimNTodim2(field, fieldDim);
 
 % Perform the correlation
-[corrmap, pmap] = corr(ts, field);
+[corrmap, pmap] = corr(ts, field, corrArgs{:});
 
 % Reshape the correlation map to the shape of the original field
 corrmap = dim2TodimN( corrmap, [1 dSize(2:end)], dOrder);
 
-% p values of NaN are not actual tests, just missing data. Thus, remove
-% them from statistical considerations.
-ptests = pmap(~isnan(pmap));
-
 % Now reshape the map of p-values
 pmap = dim2TodimN( pmap, [1 dSize(2:end)], dOrder);
-
-
-%% Perform statistical tests
-
-
-%% Test 1 - Do the finite number of tests cause true significance to fall 
-% below the desired significance level?
-
-nTests = size(ptests, 2);
-npvals = length(pvals);
-nPass = NaN(npvals, 1);
-
-% Get the number of correlations that initially pass each desired
-% significance level.
-for k = 1:npvals
-    nPass(k) = sum( ptests <= pvals(k) );
-end
-
-% Check if finite tests are still significant at each significance level
-isSig = finiteTestsAreSig( nTests, nPass, pvals);
-
-% If none of the p values are significant, stop. There is no need to
-% account for spatial reduction of degrees of freedom because none of them
-% are significant anyways.
-if ~any( isSig )
-    return;
-end
-
-
-%% Test 2 - Check if spatial correlation reduces significance below 
-% desired levels
-
-% Only run if desired
-if testSpatial
-    
-    % Begin by creating a set of random time series with the desired noise type.
-    randTS = randNoiseSeries(ts, MC, noiseType);
-
-    % Scale to the standard deviation of the time series
-    randTS = randTS * std(ts);
-
-    % Get the correlation significance of each Monte Carlo time series with
-    % the data from each point in the field
-    [~, mcPvals] = corr(randTS, field);
-
-    % For each significance level
-    mcPass = NaN( MC, npvals);
-    for k = 1:npvals
-        % For each random time series, get the number of points on the field
-        % that pass the significance level.
-        mcPass(:,k) = nansum( mcPvals < pvals(k), 2);
-
-        % Sort the number of passed tests in ascending order
-        mcPass(:,k) = sort( mcPass(:,k) );
-
-        % Get the index of the p largest number of passed tests
-        sigDex = ceil( pvals(k)*MC );
-        mcNPass = mcPass(sigDex, k);
-
-        % Compare this number of passed tests to the number passed by the time
-        % series.
-        if nPass(k) <= mcNPass
-            % If the number of passed tests does not exceed the number from
-            % the Monte Carlo simulation, then the null hypothesis is not
-            % disproved for the desired significance level
-            isSig(k) = false;
-        end
-    end
-        
-end
-
 end
 
 
 % ----- Helper Functions -----
 
 % Reads the input arguments
-function[testSpatial, noiseType, MC, fieldDim] = parseInputs( inArgs )
+function[fieldDim, corrArgs] = parseInputs( varargin )
+inArgs = varargin;
 
-% Get the number of args
-nIn = length(inArgs);
-
-% Set default fieldDim
+% Set defaults
 fieldDim = 1;
+corrArgs = {};
 
-% See if the spatial testing is suppressed
-if nIn == 0
-    error('Insufficient input arguments');
-end
-if strcmpi(inArgs{1}, 'noSpatial')
-    testSpatial = false;
-    noiseType = NaN;
-    MC = NaN;
+if ~isempty(inArgs)
+    iscorrarg = false;
+    isdim = false;
     
-    % Check for a fieldDim argument
-    if nIn >= 2
-        fieldDim = inArgs{2};
-        if nIn > 2
-            error('Too many input arguments');
-        end 
+    for k = 1:length(inArgs)
+        arg = inArgs{k};
+        
+        if iscorrarg
+            corrArgs = arg;
+            iscorrarg = false;
+        elseif isdim
+            fieldDim = arg;
+            isdim = false;
+        elseif strcmpi(arg, 'fieldDim')
+            if length(inArgs) >= k+1
+                isdim = true;
+            else
+                error('fieldDim was not specified');
+            end
+        elseif strcmpi(arg, 'corrArgs')
+            if length(inArgs) >= k+1
+                iscorrarg = true;
+            else
+                error('corrArg was not specified');
+            end
+        else
+            error('Unrecognized input');
+        end
     end
-    
-elseif nIn < 4
-    testSpatial = true;
-    MC = inArgs{1};
-    noiseType = inArgs{2};
-    
-    % Check for a fieldDim argument
-    if nIn == 3
-        fieldDim = inArgs{3};
-    end
-    
-else
-    error('Too many input arguments');
-end  
 end
-
+end
 
 % Error checking and sizes, makes ts a column vector
-function[ts] = setup(ts, field, pvals, fieldDim)
+function[ts] = errCheck(ts, field, fieldDim)
 
 % Ensure fieldDim is scalar
 if ~isscalar(fieldDim)
@@ -223,13 +119,12 @@ if ~isvector(ts)
 end
 
 % Ensure the field is at least a vector
-if isscalar(field)
-    error('field cannot be a scalar');
+if ~ismatrix(field)
+    error('field must be a matrix');
 end
 
 % Ensure that ts and field have the same lengths
 lTS = numel(ts);
-
 if lTS ~= sField(fieldDim)
     error('ts must have the same number of observations as the dimension of interest in field');
 end
@@ -238,13 +133,4 @@ end
 if isrow(ts)
     ts = ts';
 end
-
-% Ensure that pvals is a vector of values only between 0 and 1
-if ~isvector(pvals)
-    error('pvals must be a vector');    
-% Ensure the values are between 0 and 1
-elseif any(pvals<=0) || any(pvals>=1)
-    error('P values must be on the interval (0,1)');
-end
-
 end
