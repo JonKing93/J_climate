e62ac39f0e90dafef93dd703441994cc86b84b8a
JonKing93 <jonking93@gmail.com>
2017-05-11 09:19:53 -0700

    Split multComp into 2 functions -- 1 to return tests need, 1 to examine a set of pvalues.
    ALSO, replaced gammaln... for binopdf to get binomial pdf, corrects large N errors.

--- Field Correlation/multCompSig.m
+++ Field Correlation/multCompSig.m
@@ -1,21 +1,12 @@
-function[areSig, nNeeded] = multCompSig( p, varargin)
-%% Accounts for the effects of multiple comparisons to determine whether
-% multiple passed significance tests remain significant at the tested significance level.
-%
-% 
-% [areSig, nNeeded] = multCompSig( p, pvals )
-% Checks if a set of p values from multiple hypothesis tests remains above
-% the desired significance levels.
-% 
-% [areSig, nNeeded] = multCompSig( p, pvals, d )
-% Checks if a multiple sets of p values from multiple hypothesis tests
-% remain above the desired significance levels. Separate sets of p values
-% run along dimension d of pvals.
+function[varargout] = multCompSig( p, N, nPass )
+%% Returns a boolean to determine whether a finite number of passed 
+% significance tests are acutally significant at the given significance
+% levels.
 %
 % [nNeeded] = multCompSig( p, N )
 % Returns the number of passed tests needed to remain significant given the
 % number of hypothesis tests.
-% 
+%
 % [areSig, nNeeded] = multCompSig( p, N, nPass ) 
 % Returns whether a specific number of passed significance tests remains
 % significant given the number of hypothesis tests.
@@ -23,32 +14,21 @@ function[areSig, nNeeded] = multCompSig( p, varargin)
 %
 % ----- Inputs -----
 % 
-% p: The significance levels used in the tests; p must be on the interval 
-%       (0,1). If p is a vector, each significance level will be tested and
-%       each row of output will correspond to a different level.
+% p: A vector containing the tested significance levels. (e.g. p = 0.05 is 95%
+%   significant). Each p values must be on the interval (0,1)
 %
-% pvals: A set of p-values from multiple hypothesis tests. pvals may be
-%       n-dimensional. p-values must be on the interval (0,1).
+% N: The number of hypothesis tests
 %
-% d: Specifies a dimension of pvals that denotes separate sets of multiple
-%       hypothesis tests. Each set of p-values will be individually tested
-%       for significance. Each column of output will correspond to a
-%       different set of p-values.
-%
-% N: The total number of hypothesis tests. If N is a vector, p must be a
-%       vector of equal length.
-%
-% nPass: The number of passed significance tests. If nPass is a vector, N
-%       and p must be of equal length.
+% nPass: A vector containing the number of tests that pass the significance level
 %
 %
 % ----- Outputs -----
 %
-% areSig: a logical specifying whether a set of multiple p-values remain 
-%       a given significance level.
+% areSig: a boolean vector determining whether the tests still pass each
+% significance level p
 %
-% nNeeded: The minimum number of passed tests required to remain above the
-%       significance level.
+% nNeeded: The number of passed tests required to exceed each significance
+% level.
 %
 %
 % ----- References -----
@@ -61,162 +41,79 @@ function[areSig, nNeeded] = multCompSig( p, varargin)
 %
 % Jonathan King, 2017, University of Arizona, jonkin93@email.arizona.edu
 
-% Parse inputs, error check
-[pvals, d, noPvals, N, nPass] = parseInputs(p, varargin{:});
-errCheck(p, pvals, d, noPvals, N, nPass);
-
-% If pvals are given, get the value(s) of N, the total number of tests
-if ~isnan(pvals)    
-    if isnan(d)
-        N = repmat( numel(pvals(~isnan(pvals))), size(p) );
-    else
-        [N, p, pvals] = getSubsetNp(pvals, d, p);
-    end
+% Parse inputs
+if nargin == 3
+    havePass = true;
+else
+    havePass = false;
+    nPass = NaN;
 end
 
-% Get the number of tests needed for each significance level, given each N
-nNeeded = NaN( size(N) );
-for i = 1:numel(nNeeded)
+% Initial error checking
+errCheck(p, N, nPass);
 
-    % Initialize cumulative probability
-    cumProb = 0;
+nNeeded = NaN(length(p),1);
+for i = 1:length(p)
     
-    % Get each coefficient of the binomial expansion.
-    for k = N(i): -1: 0
+    % Get each binomial coefficient and add to cumulative probability
+    cumProb = 0;
+    for k = N(i):-1:0
         
-        % Use exp(gammaln...) instead of nchoosek to avoid numerical precision errors for large coefficients.
-        pnpt = exp( gammaln( N(i)+1) - gammaln(k+1) - gammaln(N(i)-k+1)) * ...
-            p(i)^k * (1-p(i))^(N(i)-k);
-
-        % Get the sum to calculate the total probability.
+        % Get the binomial probability distribution for the number of passed tests
+        pnpt = binopdf(k, N(i), p(i));
+        
+        % Add to total probability
         cumProb = pnpt + cumProb;
-
-        % When the binomial probability exceeds the desired significance level...
-        if cumProb > p
+        
+        % When the binomial probability exceeds the desired confidence level...
+        if cumProb > p(i)
             % Exit the loop
             break;
         end
-
     end
-    
-    % Previous value of k is the minimum number of passed tests needed for
-    % significance
+        
+    % Move k to the previous value, this is nNeeded for N and p
     nNeeded(i) = k+1;
 end
 
-% Get the number of passed tests if p-values were given
-if ~isnan(pvals)
-    nPass = NaN( size(N) );  % Preallocate
-    for k = 1:size(N,2)
-        
-        % Get the appropriate set of p-values
-        if isnan(d)
-            pSet = pvals;
-        else
-            pSet = pvals(k,:);
-        end
-        
-        % Compare against each value of p
-        for i = 1:size(p,1)
-            nPass(i,k) =  (  pSet(~isnan(pSet)) <= p(i)  );
-        end
-    end
-end
-        
-% Check if the number of passed tests exceeds the number of tests needed
-if noPvals && nargin==2
-    areSig = nNeeded;
-    nNeeded = NaN;
+% If the number of passed tests is given, test for significance
+if havePass
+    % Check if the number of passed tests remains significant
+    isSig = (nPass >= nNeeded);
+    varargout(1:2) = {isSig, nNeeded};
 else
-    areSig = (nPass >= nNeeded);
+    varargout = {nNeeded};
 end
+
 end
 
 % ----- Helper Functions -----
-function[N, p, pvals] = getSubsetNp(pvals, d, p)
+function[] = errCheck(p, N, nPass)
 
-% Get the indexing dimension on the first dimension
-dOrder = 1:ndims(pvals);
-dOrder(1) = d;
-dOrder(d) = 1;
-pvals = permute(pvals, dOrder);
-
-% Get the number of non-NaN entries in each subset              
-sP = size(pvals);
-nSubsets = sP(d);
-N = NaN( 1, nSubsets );
-p = repmat( p, [1, nSubsets]);
-
-for k = 1:nSubsets
-    setK = pvals(k,:);
-    N(k) = numel( setK(~isnan(setK)) );
-end
-
-N = repmat( N, [length(p),1]);
-end                    
-
-function[pvals, d, noPvals, N, nPass] = parseInputs(p, varargin)
-inArgs = varargin;
-
-% Set defaults
-pvals = NaN;
-d = NaN;
-N = NaN;
-nPass = NaN;
-
-if ~isempty(inArgs)
-    
-    arg1 = inArgs{1};
-    if ~any(  arg1(~isnan(arg1))<=0  |  arg1(~isnan(arg1))>=1  )
-        noPvals = false;
-        pvals = arg1;
-        
-    elseif ~any( arg1(~isnan(arg1) < 1) ) &&  isvector(arg1) && length(arg1)==length(p)
-        N = arg1;
-        noPvals = true;
-        
-    else
-        error('Unrecognized second input');
-    end
-    
-    if length(inArgs) == 2
-        if noPvals
-            nPass = inArgs{2};
-        else
-            d = inArgs{2};
-        end
-    elseif length(inArgs) > 2
-        error('Too many inputs');
-    end       
-else
-    error('Insufficient inputs');
-end
-
-end
-
-function[] = errCheck(p, pvals, d, noPvals, N, nPass)
-
-if p < 0 || p > 1
-    error('Significance levels (p) must be between 0 and 1');
-end
-
-if noPvals
-    if ~isnan(nPass) 
-        if(~isvector(nPass) || length(nPass)~=N)
-            error('nPass must be a vector with the same length as p and N');
-        elseif any(nPass<0 || mod(nPass,1)~=0)
-            error('nPass must be a non-negative integer');
-        end
-    end
-    if any(N<1 || mod(N,1)~=0)
-        error('N must be a positive integer');
-    end
-    
-else
-    if any( pvals(~isnan(pvals))<0) || any(pvals(~isnan(pvals))>1)
-        error('All pvals must be between 0 and 1');
-    elseif d>ndims(pvals) || d<0 || mod(d,1)~=0
-        error('d is not a valid dimension of pvals');
+if ~isvector(p)
+    error('p must be a vector');
+elseif ~isvector(N)
+    error('N must be a vector');
+elseif length(p) ~= length(N)
+    error('p and N must have the same length');
+end 
+if ~isnan(nPass)
+    if ~isvector(nPass)
+        error('nPass must be a vector');
+    elseif length(p) ~= length(nPass)
+        error('p and nPass must have the same length');
+    elseif any(nPass>N)
+        error('The number of passed tests cannot exceed the number of tests');
+    elseif any(nPass<0 | mod(nPass,1)~=0)
+        error('The number of passed tests must be a non-negative integer');
     end
 end
+
+% Ensure p is between 0 and 1
+if any(p < 0 | p > 1)
+    error('p values must be between 0 and 1');
+elseif any( N<1 | mod(N,1)~=0 )
+    error('N must be a positive integer');
+end
+
 end
---
+++ Field Correlation/pvalMultComp.m
@@ -0,0 +1,126 @@
+function[areSig, nNeeded] = pvalMultComp( p, pvals, d)
+%% Checks a collection of p-values from mutliple hypothesis testing to check
+% if they remain above a given significance level given test multiplicity
+% 
+% [areSig, nNeeded] = pvalMultComp( p, pvals )
+% Checks if a set of p values from multiple hypothesis tests remains above
+% the desired significance levels.
+% 
+% [areSig, nNeeded] = pvalMultComp( p, pvals, d )
+% Checks if a multiple sets of p values from multiple hypothesis tests
+% remain above the desired significance levels. Separate sets of p values
+% are along dimension d of pvals.
+%
+%
+% ----- Inputs -----
+% 
+% p: A vector of significance levels used for hypothesis testing. Elements 
+%       of p must be on the interval (0,1). Each row of output will
+%       correspond to a significance level.
+%
+% pvals: A set of p-values from multiple hypothesis tests. pvals may be
+%       n-dimensional. If pvals contains multiple sets of p-values, the 
+%       subsets must run along a common dimension. Elements of pvals must
+%       be on the interval (0,1).
+%
+% d: Specifies a dimension of pvals that denotes separate sets of multiple
+%       hypothesis tests. Each set of p-values will be individually tested
+%       for significance. Each column of output will correspond to a
+%       different set of p-values.
+%
+%
+% ----- Outputs -----
+%
+% areSig: a logical specifying whether a set of multiple p-values remain 
+%       above given significance level.
+%
+% nNeeded: The minimum number of passed tests required to remain above the
+%       significance level.
+%
+%
+% ----- References -----
+%
+% Livezey, R. E., & Chen, W. Y. (1983). Statistical field significance and 
+%   its determination by Monte Carlo techniques. Monthly Weather Review, 111(1), 46-59.
+%
+%
+% ----- Written By -----
+%
+% Jonathan King, 2017, University of Arizona, jonkin93@email.arizona.edu
+
+% Parse inputs
+if nargin == 2
+    singleSet = true;
+    d = NaN;
+else
+    singleSet = false;
+end
+
+% Error check, make everything a column vector
+[p] = errCheck(p, pvals, d);
+
+% Make the indexing dimension first for multiple sets of p-values
+if ~singleSet
+    dOrder = 1:ndims(pvals);
+    dOrder(1) = d;
+    dOrder(d) = 1;
+    pvals = permute(pvals, dOrder);
+end
+
+% Get the number of hypothesis tests (N) for each set of p-values
+if singleSet
+    N = repmat( numel(pvals(~isnan(pvals))), size(p) );
+    nSubsets = 1;
+else
+    % Get N for individual subsets
+    nSubsets = size(pvals,1);
+    N = NaN(length(p), nSubsets);
+    
+    for k = 1:nSubsets
+        setK = pvals(k,:);
+        N(:,k) = numel( setK(~isnan(setK)) );
+    end
+end
+
+% Get the number of tests needed for each significance level and N
+nNeeded = NaN(size(N));
+for k = 1:nSubsets
+    nNeeded(:,k) = multCompSig(p, N(:,k));
+end
+
+% Check how many tests are passed
+nPass = NaN( size(N) );
+if singleSet
+    for k = 1:length(p)
+        nPass(k) = numel( pvals(~isnan(pvals)) <= p(k) );
+    end
+else
+    for j = 1:nSubsets
+        mapJ = pvals(j,:);
+        for k = 1:length(p)
+            nPass(k,j) = numel( mapJ(~isnan(mapJ)) <= p(k) );
+        end
+    end
+end
+    
+% Check if the number of passed tests is sufficient to maintain
+% significance
+areSig =  (nPass >= nNeeded);     
+
+end               
+
+function[p] = errCheck(p, pvals, d)
+if any(p < 0 | p > 1)
+    error('Significance levels (p) must be between 0 and 1');
+elseif any(pvals(~isnan(pvals))<0 | pvals(~isnan(pvals))>1)
+    error('pvals must be on the interval (0,1)');
+end
+
+if ~isnan(d) && (d>ndims(pvals) || d<0 || mod(d,1)~=0)
+    error('d is not a valid dimension of pvals');
+end
+
+if isrow(p)
+    p = p';
+end
+end
